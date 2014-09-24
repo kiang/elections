1bb11a6b6b2d218247b57dc13d1a8855e3f85eb5
kiang <kiange@gmail.com>
2014-09-24 14:37:35 +0800

    script to dump db records

---
+++ Console/Command/ExportShell.php
@@ -0,0 +1,64 @@
+<?php
+
+class ExportShell extends AppShell {
+
+    public $uses = array('Election');
+
+    public function main() {
+        $this->candidates();
+    }
+
+    public function candidates() {
+        $rootNode = $this->Election->find('first', array(
+            'conditions' => array('Election.name' => '2014'),
+        ));
+        $eTypes = $this->Election->find('all', array(
+            'fields' => array('id', 'name', 'lft', 'rght'),
+            'conditions' => array('Election.parent_id' => $rootNode['Election']['id']),
+        ));
+        $fh = fopen(__DIR__ . '/data/db_dump.csv', 'w');
+        foreach ($eTypes AS $eType) {
+            $eNodes = $this->Election->find('all', array(
+                'fields' => array('id', 'name'),
+                'conditions' => array(
+                    'Election.rght - Election.lft = 1',
+                    'Election.rght <' => $eType['Election']['rght'],
+                    'Election.lft >' => $eType['Election']['lft'],
+                ),
+            ));
+            foreach ($eNodes AS $eNode) {
+                $parents = $this->Election->getPath($eNode['Election']['id'], array('id', 'name'));
+                unset($parents[0]);
+                unset($parents[1]);
+                $eNode['Election']['name'] = implode(' > ', Set::extract($parents, '{n}.Election.name'));
+                $candidates = $this->Election->Candidate->find('all', array(
+                    'conditions' => array(
+                        'Candidate.active_id IS NULL',
+                        'CandidatesElection.Election_id' => $eNode['Election']['id'],
+                    ),
+                    'fields' => array('Candidate.id', 'Candidate.name'),
+                    'joins' => array(
+                        array(
+                            'table' => 'candidates_elections',
+                            'alias' => 'CandidatesElection',
+                            'type' => 'inner',
+                            'conditions' => array(
+                                'CandidatesElection.Candidate_id = Candidate.id',
+                            ),
+                        ),
+                    )
+                ));
+                foreach ($candidates AS $candidate) {
+                    fputcsv($fh, array(
+                        $eType['Election']['name'],
+                        $eNode['Election']['name'],
+                        $candidate['Candidate']['name'],
+                        'http://k.olc.tw/elections/candidates/view/' . $candidate['Candidate']['id'],
+                    ));
+                }
+            }
+        }
+        fclose($fh);
+    }
+
+}
