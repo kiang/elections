d26184b33321bbe1b1941e6e40c0ebc940beb3b8
kiang@w540 <kiange@gmail.com>
2015-01-07 01:11:08 +0800

    dump candidates with lawsuit tags

--- Console/Command/ExportShell.php
+++ Console/Command/ExportShell.php
@@ -5,7 +5,99 @@ class ExportShell extends AppShell {
     public $uses = array('Election');
 
     public function main() {
-        $this->facebook();
+        $this->lawsuitCandidates();
+    }
+
+    public function lawsuitCandidates() {
+        $tags = $this->Election->Candidate->Tag->find('all', array(
+            'conditions' => array('Tag.name LIKE' => '當選無效之訴%'),
+            'contain' => array(
+                'Candidate' => array(
+                    'fields' => array('id', 'name', 'no', 'party'),
+                    'CandidatesElection',
+                ),
+            ),
+            'order' => array(
+                'Tag.name' => 'ASC',
+            ),
+        ));
+        $elections = array();
+        $fh = fopen(__DIR__ . '/data/2014_lawsuit_candidates.csv', 'w');
+        fputcsv($fh, array(
+            '標籤',
+            '政黨',
+            '編號',
+            '候選人',
+            '選區',
+        ));
+        foreach ($tags AS $tag) {
+            $lines = array();
+            foreach ($tag['Candidate'] AS $candidate) {
+                $electionId = $candidate['CandidatesElection'][0]['Election_id'];
+                if (!isset($elections[$electionId])) {
+                    $path = $this->Election->getPath($electionId, array('name'));
+                    unset($path[0]);
+                    unset($path[2]);
+
+                    $elections[$electionId] = implode(' > ', Set::extract('{n}.Election.name', $path));
+                    if (false !== strpos($elections[$electionId], '> 第')) {
+                        $areas = $this->Election->Area->find('list', array(
+                            'joins' => array(
+                                array(
+                                    'table' => 'areas_elections',
+                                    'alias' => 'AreasElection',
+                                    'type' => 'inner',
+                                    'conditions' => array(
+                                        'AreasElection.Area_id = Area.id',
+                                    ),
+                                ),
+                            ),
+                            'conditions' => array(
+                                'AreasElection.Election_id' => $electionId,
+                            ),
+                        ));
+                        $elections[$electionId] .= '(' . implode('/', $areas) . ')';
+                    }
+                }
+                switch (mb_substr($elections[$electionId], 0, 4, 'utf8')) {
+                    case '直轄市議':
+                        $key = 1;
+                        break;
+                    case '縣市議員':
+                        $key = 2;
+                        break;
+                    case '直轄市山':
+                        $key = 3;
+                        break;
+                    case '鄉鎮市長':
+                        $key = 4;
+                        break;
+                    case '鄉鎮市民':
+                        $key = 5;
+                        break;
+                    case '村里長 ':
+                        $key = 6;
+                        break;
+                }
+                if (!isset($lines[$key])) {
+                    $lines[$key] = array();
+                }
+                $lines[$key][] = array(
+                    $tag['Tag']['name'],
+                    $candidate['party'],
+                    $candidate['no'],
+                    $candidate['name'],
+                    $elections[$electionId],
+                );
+            }
+            ksort($lines);
+            foreach ($lines AS $c) {
+                foreach ($c AS $line) {
+                    fputcsv($fh, $line);
+                }
+            }
+        }
+        fclose($fh);
     }
 
     public function facebook() {
@@ -14,7 +106,7 @@ class ExportShell extends AppShell {
             'conditions' => array(
                 'Candidate.links LIKE' => '%facebook.com%',
                 'Candidate.active_id IS NULL',
-                ),
+            ),
             'contain' => array('Election'),
         ));
         $fh = fopen(__DIR__ . '/data/facebook_candidates.csv', 'w');
