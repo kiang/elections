f6dcf6d59a782853a875d6d5ba139c4e1d46af6d
kiang <kiange@gmail.com>
2015-12-27 13:12:46 +0800

    add doc and update data

--- Console/Command/data/2016_candidates/api/download.php
+++ Console/Command/data/2016_candidates/api/download.php
@@ -1,4 +1,7 @@
 <?php
+/*
+ * api doc( ./doc.pdf ): http://2016.cec.gov.tw/upload/file/2015-11-18/e0c73593-c229-4f5d-80d9-c62691286fd9/aad62a286d8d77f92fe8eebff1f3af7e.pdf
+ */
 for($i = 1; $i <=8; $i++) {
     file_put_contents(__DIR__ . '/' . $i . '.json', file_get_contents('http://2016.cec.gov.tw/opendata/cec2016/getJson?dataType=' . $i));
 }
